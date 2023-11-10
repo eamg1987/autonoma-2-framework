4fc37dfc704631f660a0324c34f7d57109307d93
eamg1987 <eamacias4@tes.edu.ec>
2023-11-09 23:49:34 -0500

    utilizando LINQ y EF Core.

--- basededatos/Program.cs
+++ basededatos/Program.cs
@@ -1,120 +1,58 @@
-﻿using System;
+﻿
 using System.Data;
-using System.Drawing;
-using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
 using MySql.Data.MySqlClient;
-using Org.BouncyCastle.Asn1.X509;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+public class Alumno
+{
+    [Key]
+    public int id_alumno { get; set; }
+    public string nombres { get; set; }
+    public string apellidos { get; set; }
+    public int edad { get; set; }
+    public string sexo { get; set; }
+    public string email { get; set; }
+    public string telefono { get; set; }
+}
+public class AppDbContext : DbContext
+{
+    public DbSet<Alumno> Alumnos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Reemplaza "Server=localhost;Database=autonoma2;Uid=root;Pwd='';" con tu cadena de conexión correcta
+        optionsBuilder.UseMySQL("Server=localhost;Database=autonoma2;Uid=root;Pwd='';");
+    }
+}
 
 class Program
 {
     static void Main()
     {
-        string connectionString = "Server=localhost;Database=autonoma2;Uid=root;Pwd='';";
-
-        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        using (var context = new AppDbContext())
         {
             try
             {
-                connection.Open();
+                // Consulta numero 1 - Todos los datos
+                var consulta1 = context.Alumnos.ToList();
 
-                // Aquí puedes ejecutar tus consultas
-                //consulta numero 1 todos los datos
-                string query = "SELECT * FROM alumnos";
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-
-                using (MySqlDataReader reader = cmd.ExecuteReader())
+                Console.WriteLine("Consulta numero 1 - Todos los datos");
+                foreach (var alumno in consulta1)
                 {
-                    Console.WriteLine("Consulta numero 1 todos los datos");
-                    while (reader.Read())
-                    {
-                        Console.WriteLine("Nombres:  " + reader["nombres"].ToString());
-                        Console.WriteLine("Apellidos:  " + reader["apellidos"].ToString());
-                        Console.WriteLine("Edad:  " + reader["edad"].ToString());
-                        Console.WriteLine("Sexo:  " + reader["sexo"].ToString());
-                        Console.WriteLine("Email:  " + reader["email"].ToString());
-                        Console.WriteLine("Telefono:  " + reader["telefono"].ToString());
-                    }
+                    Console.WriteLine($"Nombres: {alumno.nombres}");
+                    Console.WriteLine($"Apellidos: {alumno.apellidos}");
+                    Console.WriteLine($"Edad: {alumno.edad}");
+                    Console.WriteLine($"Sexo: {alumno.sexo}");
+                    Console.WriteLine($"Email: {alumno.email}");
+                    Console.WriteLine($"Telefono: {alumno.telefono}");
                 }
-                //Consulta numero 2 seleccionando solo dos columnas
-                string query2 = "SELECT nombres, apellidos FROM alumnos where edad >10";
-                MySqlCommand cmd2 = new MySqlCommand(query2, connection);
-
-                using (MySqlDataReader reader = cmd2.ExecuteReader())
-                {
-                    Console.WriteLine("===============================================");
-                    Console.WriteLine("Consulta numero 2 seleccionando solo dos columnas ");
-                    while (reader.Read())
-                    {
-                        Console.WriteLine("Nombres:  " + reader["nombres"].ToString());
-                        Console.WriteLine("Apellidos:  " + reader["apellidos"].ToString());
-
-                    }
-
-                }
-                //consulta numero 3 usando order by
-                string query3 = "SELECT * FROM alumnos order by edad";
-                MySqlCommand cmd3 = new MySqlCommand(query3, connection);
-
-                using (MySqlDataReader reader = cmd3.ExecuteReader())
-                {
-                    Console.WriteLine("===============================================");
-                    Console.WriteLine("Consulta numero 3 usando order by por edad");
-                    while (reader.Read())
-                    {
-                        Console.WriteLine("Nombres:  " + reader["nombres"].ToString());
-                        Console.WriteLine("Apellidos:  "+ reader["apellidos"].ToString());
-                        Console.WriteLine("Edad:  "+ reader["edad"].ToString());
-                        Console.WriteLine("Sexo:  " + reader["sexo"].ToString());
-                        Console.WriteLine("Email:  " + reader["email"].ToString());
-                        Console.WriteLine("Telefono:  " + reader["telefono"].ToString());
-
-                    }
-
-                }
-                //consulta numero 4 usando GROUP BY
-                string query4 = "SELECT sexo, COUNT(*) as total FROM alumnos group by sexo";
-                MySqlCommand cmd4 = new MySqlCommand(query4, connection);
-
-                using (MySqlDataReader reader = cmd4.ExecuteReader())
-                {
-                    Console.WriteLine("===============================================");
-                    Console.WriteLine("Consulta numero 4 usando Group by");
-                    while (reader.Read())
-                    {                
-                        Console.WriteLine("Sexo:  " + reader["sexo"].ToString());
-                        Console.WriteLine("Total:  " + reader["total"].ToString());
-                    }
-
-                }
-                //Una consulta con WHERE, GROUP BY Y ORDER BY
-                string query5 = "SELECT edad, COUNT(*) as cantidad_alumnos FROM alumnos where edad>20 GROUP BY edad ORDER BY edad;";
-                MySqlCommand cmd5 = new MySqlCommand(query5, connection);
-
-                using (MySqlDataReader reader = cmd5.ExecuteReader())
-                {
-                    Console.WriteLine("===============================================");
-                    Console.WriteLine("Consulta numero 5  con WHERE, GROUP BY Y ORDER BY");
-                    while (reader.Read())
-                    {
-                        Console.WriteLine("Edad:  " + reader["edad"].ToString());
-                        Console.WriteLine("Cantidad de alumnos:  " + reader["cantidad_alumnos"].ToString());
-                    }
-
-                }
-                
             }
-            catch (MySqlException ex)
+            catch (Exception ex)
             {
-                Console.WriteLine("Error de MySQL: " + ex.Message);
-            }
-            finally
-            {
-                // Asegúrate de cerrar la conexión en el bloque finally
-                if (connection.State != ConnectionState.Closed)
-                {
-                    connection.Close();
-                }
+                Console.WriteLine("Error: " + ex.Message);
             }
         }
     }
