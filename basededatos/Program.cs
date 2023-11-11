d353bf2fdf558430cd8ee3c71e0046cde78072b8
eamg1987 <eamacias4@tes.edu.ec>
2023-11-10 20:14:18 -0500

    autonoma 1 completada

--- basededatos/Program.cs
+++ basededatos/Program.cs
@@ -49,19 +49,83 @@ class Program
                     Console.WriteLine($"Email: {alumno.email}");
                     Console.WriteLine($"Telefono: {alumno.telefono}");
                 }
-                // Consulta numero 2 - solo dos columnas
-                var resultadoConsulta = context.Alumnos
-                    .Where(a => a.edad >= 18) // Filtro con WHERE
-                    .GroupBy(a => a.sexo)      // Agrupar por Sexo
+                // Consulta numero 2 - solo 2 columnas
+                var consulta2 = context.Alumnos
+                    .Where(a => a.edad >= 20) // Filtro con WHERE
+                    .OrderBy(a => a.nombres)
                     .Select(grupo => new
                     {
-                        Sexo = grupo.Key,
-                        Cantidad = grupo.Count()
+                        Nombres = grupo.nombres,
+                        Apellidos = grupo.apellidos
                         // Puedes agregar más propiedades según tus necesidades
                     })
                     .ToList();
 
                 // Imprimir resultados
+                Console.WriteLine("===============================================");
+                Console.WriteLine("Consulta numero 2 - solo 2 columnas");
+                foreach (var resultado in consulta2)
+                {
+                    Console.WriteLine($"Nombres: {resultado.Nombres}, Apellidos: {resultado.Apellidos}");
+                }
+
+                // Consulta numero 3 -  con ORDER BY
+                var consulta3 = context.Alumnos
+                    .Where(a => a.sexo == "Masculino") // Filtro con WHERE
+                    .OrderBy(a => a.edad)
+                    .ToList();
+
+                // Imprimir resultados
+                Console.WriteLine("===============================================");
+                Console.WriteLine("Consulta numero 3 - con ORDER BY");
+                foreach (var resultado in consulta3)
+                {
+                    Console.WriteLine($"Nombres: {resultado.nombres}");
+                    Console.WriteLine($"Apellidos: {resultado.apellidos}");
+                    Console.WriteLine($"Edad: {resultado.edad}");
+                    Console.WriteLine($"Sexo: {resultado.sexo}");
+                    Console.WriteLine($"Email: {resultado.email}");
+                    Console.WriteLine($"Telefono: {resultado.telefono}");
+                }
+
+                // Consulta numero 4 -  con Groupby
+                var consulta4 = context.Alumnos
+                    .Where(a => a.sexo == "femenino") // Filtro con WHERE
+                    .GroupBy(a => a.edad)
+                    .Select( group =>new
+                    {
+                        Edad = group.Key,
+                        Total= group.Count()
+                    }
+                    )
+                    .ToList();
+
+                // Imprimir resultados
+                Console.WriteLine("===============================================");
+                Console.WriteLine("Consulta numero 4 - con GROUP BY");
+                foreach (var resultado in consulta4)
+                {
+                    Console.WriteLine($"Edad: {resultado.Edad}");
+                    Console.WriteLine($"Total: {resultado.Total}");
+                }
+
+
+                // Consulta numero 5 -  con WHERE,  GROUP BY  Y ORDER BY
+                var resultadoConsulta = context.Alumnos
+                    .Where(a => a.edad >= 18) // Filtro con WHERE
+                    .GroupBy(a => a.sexo)    // Agrupar por Sexo
+                    .OrderBy(a => a.Key)
+                    .Select(grupo => new
+                    {
+                        Sexo = grupo.Key,
+                        Cantidad = grupo.Count()
+                        
+                    })
+                    .ToList();
+
+                // Imprimir resultados
+                Console.WriteLine("===============================================");
+                Console.WriteLine("Consulta numero 5 - con WHERE,  GROUP BY  Y ORDER BY");
                 foreach (var resultado in resultadoConsulta)
                 {
                     Console.WriteLine($"Sexo: {resultado.Sexo}, Cantidad: {resultado.Cantidad}");
